bbe4e27cc7e29bd3771303bbe8e0fc1c976e2ece
Sarthak Gaikwad <123548573+Sarthak5290@users.noreply.github.com>
2024-12-09 17:54:15 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 // MongoDB connection utility
 const connectToDatabase = async () => {
+  // Check if the connection already exists (for serverless environments)
   if (mongoose.connections[0].readyState) {
     console.log("Already connected to MongoDB");
     return;
@@ -9,7 +10,10 @@ const connectToDatabase = async () => {
 
   try {
     console.log("Connecting to MongoDB...");
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connection successful.");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
